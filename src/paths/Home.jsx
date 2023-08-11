0c13f1e661a1289957933d7901d9dfcd201e769a
Emmanuel Opoku-Ameyaw <ameyawemmanuel900@gmail.com>
2023-08-11 20:39:34 +0000

    Files added

---
+++ src/paths/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import BannerImage from "../assets/hero-banner.svg";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <main className="hero">
+      <div className="hero__intro">
+        <h1>One place for all your favourite movies and shows</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut maxime
+          ea suscipit, labore minus error dolor quas quod exercitationem
+          tempora, fugiat ex repellendus enim in!
+        </p>
+      </div>
+      <img
+        src={BannerImage}
+        alt="video illustration"
+        style={{ width: "70%" }}
+      />
+      <div className="hero__buttons">
+        <Link to="/movies" className="hero__buttons--explore">Explore Movies</Link>
+        <Link to="signup">Signup Now</Link>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
