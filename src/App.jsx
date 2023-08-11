2198b1976945f25b03da080cc5c0a590d4179b62
Emmanuel Opoku-Ameyaw <ameyawemmanuel900@gmail.com>
2023-08-11 20:38:19 +0000

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 // COmponents lazilt loaded
 const Navbar = lazy(() => import("./paths/Navbar"));
+const Home = lazy(() => import("./paths/Home"));
 
 function App() {
   return (
     <>
       <Navbar />
+      <Suspense fallback={<h1 className="loading">Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- src/paths/Navbar.jsx
+++ src/paths/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  // Functions
+  function handleBurgerEvent() {
+    setShowMobileNav((prev) => !prev);
+    console.log(showMobileNav);
+  }
+
   return (
     <nav className="nav">
       <h1>VideoPanel</h1>
       <div className="nav__search">
-        <input type="search" placeholder="Search a movie..." value={"hello"}/>
+        <input type="search" placeholder="Search a movie..." value={"hello"} />
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </div>
-      <ul className="nav__items">
+      <ul
+        className="nav__items"
+        style={{ transform: showMobileNav ? "translateX(0)" : null }}
+      >
         <li className="nav__items--item">
           <Link to="/" className="link">
             Home
@@ -29,7 +39,10 @@ const Navbar = () => {
         </li>
       </ul>
       {/* Hmaburger menu */}
-      <div className="nav__hamburger">
+      <div
+        className={showMobileNav ? "nav__hamburger active" : "nav__hamburger"}
+        onClick={handleBurgerEvent}
+      >
         <span className="burger burger1"></span>
         <span className="burger burger2"></span>
         <span className="burger burger3"></span>
